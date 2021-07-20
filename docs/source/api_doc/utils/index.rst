d01fd02a6ac6958edc317a5b0fbc2983fb7185a1
HansBug <killog@126.com>
2021-07-21 01:53:04 +0800

    add documentation for treevalue.utils.enum

--- docs/source/api_doc/utils/index.rst
+++ docs/source/api_doc/utils/index.rst
@@ -4,5 +4,6 @@ treevalue.utils
 .. toctree::
     :maxdepth: 3
 
+    enum
     final
     func
--- treevalue/utils/enum.py
+++ treevalue/utils/enum.py
@@ -25,6 +25,75 @@ def _get_default_external_preprocess(enum_class: Type[_EnumType]):
 def int_enum_loads(enable_int: bool = True, value_preprocess: Optional[Callable[[int, ], int]] = None,
                    enable_str: bool = True, name_preprocess: Optional[Callable[[str, ], str]] = None,
                    external_process: Optional[Callable[[Any, ], Optional[_EnumType]]] = None):
+    """
+    Overview:
+        Decorate a int enum class with a new `loads` class method.
+
+    Arguments:
+        - enable_int (:obj:`bool`): Enable int parse, default is `True`.
+        - value_preprocess (:obj:`Optional[Callable[[int, ], int]]`): Preprocessor of value, \
+            default is `None` which means no change.
+        - enable_str (:obj:`bool`): Enable str parse, default is `True`.
+        - name_preprocess (:obj:`Optional[Callable[[str, ], str]]`): Preprocessor of name, \
+            default is `None` which means no change.
+        - external_process (:obj:`Optional[Callable[[Any, ], Optional[_EnumType]]]`): External processor \
+            for the unprocessable data, default is `None` which means raise a `KeyError`.
+
+    Examples:
+        - Simple usage
+
+        >>> from enum import IntEnum, unique
+        >>>
+        >>> @int_enum_loads()
+        >>> @unique
+        >>> class MyEnum(IntEnum):
+        >>>     A = 1
+        >>>     B = 2
+        >>>
+        >>> MyEnum.loads(1)    # MyEnum.A
+        >>> MyEnum.loads('A')  # MyEnum.A
+        >>> MyEnum.loads(2)    # MyEnum.B
+        >>> MyEnum.loads('B')  # MyEnum.B
+        >>> MyEnum.loads(-1)   # KeyError
+        >>> MyEnum.loads('a')  # KeyError
+        >>> MyEnum.loads('C')  # KeyError
+
+        - Preprocessors
+
+        >>> from enum import IntEnum, unique
+        >>>
+        >>> @int_enum_loads(name_preprocess=str.upper, value_preprocess=abs)
+        >>> @unique
+        >>> class MyEnum(IntEnum):
+        >>>     A = 1
+        >>>     B = 2
+        >>>
+        >>> MyEnum.loads(1)    # MyEnum.A
+        >>> MyEnum.loads('A')  # MyEnum.A
+        >>> MyEnum.loads(2)    # MyEnum.B
+        >>> MyEnum.loads('B')  # MyEnum.B
+        >>> MyEnum.loads(-1)   # MyEnum.A
+        >>> MyEnum.loads('a')  # MyEnum.A
+        >>> MyEnum.loads('C')  # KeyError
+
+        - External processor
+
+        >>> from enum import IntEnum, unique
+        >>>
+        >>> @int_enum_loads(external_process=lambda data: None)
+        >>> @unique
+        >>> class MyEnum(IntEnum):
+        >>>     A = 1
+        >>>     B = 2
+        >>>
+        >>> MyEnum.loads(1)    # MyEnum.A
+        >>> MyEnum.loads('A')  # MyEnum.A
+        >>> MyEnum.loads(2)    # MyEnum.B
+        >>> MyEnum.loads('B')  # MyEnum.B
+        >>> MyEnum.loads(-1)   # None
+        >>> MyEnum.loads('a')  # None
+        >>> MyEnum.loads('C')  # None
+    """
     value_preprocess = value_preprocess or _default_value_preprocess
     name_preprocess = name_preprocess or _default_name_preprocess
 
