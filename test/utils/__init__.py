508f7438cd4212d27290643e13ddafa0f395a141
HansBug <killog@126.com>
2021-07-21 01:54:13 +0800

    remove ueseless treevalue.utils.value

--- test/utils/__init__.py
+++ test/utils/__init__.py
@@ -3,4 +3,3 @@ from .test_enum import TestUtilsEnum
 from .test_final import TestUtilsFinal
 from .test_func import TestUtilsFunc
 from .test_singleton import TestUtilsSingleton
-from .test_value import TestUtilsValue
--- treevalue/utils/__init__.py
+++ treevalue/utils/__init__.py
@@ -3,4 +3,3 @@ from .enum import int_enum_loads
 from .final import FinalMeta
 from .func import args_iter
 from .singleton import SingletonMeta, ValueBasedSingletonMeta, SingletonMark
-from .value import StaticValueProxy, ValueProxy
