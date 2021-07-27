b0a27b68429d67d124efb59d8878a00734e55f40
HansBug <killog@126.com>
2021-07-27 08:58:26 +0800

    update install_check_demo.py

--- docs/source/tutorials/install_check.demo.py
+++ docs/source/tutorials/install_check.demo.py
@@ -1,4 +1,5 @@
-from treevalue.config.meta import __VERSION__
+from treevalue.config.meta import __VERSION__, __TITLE__, __AUTHOR__
 
 if __name__ == '__main__':
-    print(__VERSION__)
+    print(__TITLE__, __VERSION__)
+    print("Powered by", __AUTHOR__)
