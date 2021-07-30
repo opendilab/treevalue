93f5223cf51349c191c003b5491b18466ae1f338
HansBug <killog@126.com>
2021-07-30 22:26:48 +0800

    use abs path in diagrams and dots

--- docs/source/diagrams.mk
+++ docs/source/diagrams.mk
@@ -6,10 +6,10 @@ PNGS   := $(addsuffix .puml.png, $(basename ${PUMLS}))
 SVGS   := $(addsuffix .puml.svg, $(basename ${PUMLS}))
 
 %.puml.png: %.puml
-	$(PLANTUMLCLI) -t png -o $@ $<
+	$(PLANTUMLCLI) -t png -o $(shell readlink -f $@) $(shell readlink -f $<)
 
 %.puml.svg: %.puml
-	$(PLANTUMLCLI) -t svg -o $@ $<
+	$(PLANTUMLCLI) -t svg -o $(shell readlink -f $@) $(shell readlink -f $<)
 
 build: ${SVGS} ${PNGS}
 
