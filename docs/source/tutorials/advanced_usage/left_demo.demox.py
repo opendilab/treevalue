4d8f8b39ec355dc65aa72cedd224473222940ad2
HansBug <killog@126.com>
2021-08-01 01:10:37 +0800

    complete left mode documentation

--- docs/source/tutorials/advanced_usage/left_demo.demox.py
+++ docs/source/tutorials/advanced_usage/left_demo.demox.py
@@ -1,13 +1,13 @@
 from treevalue import FastTreeValue, func_treelize
 
 
-@func_treelize(mode='strict')
+@func_treelize(mode='left')
 def plus(a, b):
     return a + b
 
 
 if __name__ == '__main__':
-    t1 = FastTreeValue({'a': 1, 'b': 2, 'x': {'c': 3, 'd': 4, 'e': 5}})
+    t1 = FastTreeValue({'b': 2, 'x': {'c': 3, 'd': 4, 'e': 5}})
     t2 = FastTreeValue({'a': 11, 'b': 22, 'x': {'c': 30, 'd': 48, 'e': 54}})
     t4 = FastTreeValue({'b': 2, 'x': {'c': 3, 'd': 4, 'e': 5, 'f': 6}})
 
