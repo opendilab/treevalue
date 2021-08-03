055fed322bdc615ccfbfeee0a9b440fbfb373075
HansBug <killog@126.com>
2021-08-03 13:10:48 +0800

    update test for graphviz

--- test/utils/test_tree.py
+++ test/utils/test_tree.py
@@ -21,17 +21,21 @@ class TestUtilsTree:
         g = build_graph((t, 't'), graph_title="Demo of build_graph.")
         assert "Demo of build_graph." in g.source
         assert "t.x" in g.source
+        assert 530 < len(g.source) <= 560
 
         g2 = build_graph(t, graph_title="Demo 2 of build_graph.")
         assert "Demo 2 of build_graph." in g2.source
         assert "<root_0>" in g2.source
         assert "<root_0>.x" in g2.source
+        assert 550 < len(g2.source) <= 580
 
         g3 = build_graph((t,), graph_title="Demo 3 of build_graph.")
         assert "Demo 3 of build_graph." in g3.source
         assert "<root_0>" in g3.source
         assert "<root_0>.x" in g3.source
+        assert 550 < len(g3.source) <= 580
 
         g4 = build_graph((), graph_title="Demo 4 of build_graph.")
         assert "Demo 4 of build_graph." in g4.source
         assert "node" not in g4.source
+        assert 90 <= len(g4.source) <= 110
