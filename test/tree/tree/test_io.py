e8618619c9c7048ac3b1ec2c2725383100e3511f
HansBug <killog@126.com>
2021-08-05 15:38:42 +0800

    update compress support

--- test/tree/tree/test_io.py
+++ test/tree/tree/test_io.py
@@ -9,30 +9,69 @@ class MyTreeValue(FastTreeValue):
     pass
 
 
+# noinspection DuplicatedCode
 @pytest.mark.unittest
 class TestTreeTreeIo:
     def test_dumps_and_loads(self):
-        t = FastTreeValue({'a': 1, 'b': 2, 'x': {'c': 3, 'd': 4}})
+        t = FastTreeValue({'a': 1, 'b': 2, 'x': {'c': [3] * 1000, 'd': 4}})
 
-        t1 = loads(dumps(t), type_=FastTreeValue)
-        assert len(dumps(t)) < 130
+        _dumped_data = dumps(t)
+        t1 = loads(_dumped_data, type_=FastTreeValue)
+        assert 2130 < len(_dumped_data) < 2150
         assert isinstance(t1, FastTreeValue)
         assert t1 == t
 
-        t2 = loads(dumps(t), type_=MyTreeValue)
+        t2 = loads(_dumped_data, type_=MyTreeValue)
         assert isinstance(t2, MyTreeValue)
         assert t2 != t
         assert FastTreeValue(t2) == t
+
+        with pytest.warns(UserWarning):
+            t1 = loads(_dumped_data, decompress=zlib.decompress, type_=FastTreeValue)
+        assert isinstance(t1, FastTreeValue)
+        assert t1 == t
 
     def test_dumps_and_loads_with_zip(self):
-        t = FastTreeValue({'a': 1, 'b': 2, 'x': {'c': 3, 'd': 4}})
+        t = FastTreeValue({'a': 1, 'b': 2, 'x': {'c': [3] * 1000, 'd': 4}})
 
-        t1 = loads(dumps(t, compress=zlib.compress), type_=FastTreeValue, decompress=zlib.decompress)
-        assert len(dumps(t, compress=zlib.compress)) < 105
+        _dumped_data = dumps(t, compress=zlib)
+        t1 = loads(_dumped_data, type_=FastTreeValue)
+        assert 210 < len(dumps(t, compress=zlib)) < 220
         assert isinstance(t1, FastTreeValue)
         assert t1 == t
 
-        t2 = loads(dumps(t, compress=zlib.compress), type_=MyTreeValue, decompress=zlib.decompress)
+        t2 = loads(_dumped_data, type_=MyTreeValue)
         assert isinstance(t2, MyTreeValue)
         assert t2 != t
         assert FastTreeValue(t2) == t
+
+        with pytest.warns(UserWarning):
+            t1 = loads(_dumped_data, decompress=zlib.decompress, type_=FastTreeValue)
+        assert isinstance(t1, FastTreeValue)
+        assert t1 == t
+
+    def test_dumps_and_loads_with_zip_tuple(self):
+        t = FastTreeValue({'a': 1, 'b': 2, 'x': {'c': [3] * 1000, 'd': 4}})
+
+        _dumped_data = dumps(t, compress=(zlib.compress, zlib.decompress))
+        t1 = loads(_dumped_data, type_=FastTreeValue)
+        assert 210 < len(_dumped_data) < 220
+        assert isinstance(t1, FastTreeValue)
+        assert t1 == t
+
+        t2 = loads(_dumped_data, type_=MyTreeValue)
+        assert isinstance(t2, MyTreeValue)
+        assert t2 != t
+        assert FastTreeValue(t2) == t
+
+    def test_dumps_and_loads_with_compress_only(self):
+        t = FastTreeValue({'a': 1, 'b': 2, 'x': {'c': [3] * 1000, 'd': 4}})
+
+        _dumped_data = dumps(t, compress=zlib.compress)
+        t1 = loads(_dumped_data, decompress=zlib.decompress, type_=FastTreeValue)
+        assert 130 < len(_dumped_data) < 150
+        assert isinstance(t1, FastTreeValue)
+        assert t1 == t
+
+        with pytest.raises(RuntimeError):
+            loads(_dumped_data, type_=FastTreeValue)
--- treevalue/tree/tree/io.py
+++ treevalue/tree/tree/io.py
@@ -1,14 +1,31 @@
 import io
 import pickle
+import warnings
+import zlib
 from typing import Optional, Type, TypeVar, Union, Callable
 
+import dill
+
 from .tree import TreeValue, get_data_property
-from ...utils import dynamic_call
 
 _TreeType = TypeVar('_TreeType', bound=TreeValue)
 
+_NO_DECOMPRESSION = 0
+_NEED_DECOMPRESSION = 1
 
-def dump(t: _TreeType, file, compress: Optional[Callable] = None):
+
+def _extract_compress_and_decompress(compress):
+    if isinstance(compress, tuple):
+        return compress[0], compress[1]
+    elif hasattr(compress, '__call__'):
+        return compress, _NEED_DECOMPRESSION
+    elif compress is None:
+        return (lambda x: x), _NO_DECOMPRESSION
+    else:
+        return getattr(compress, 'compress'), getattr(compress, 'decompress')
+
+
+def dump(t: _TreeType, file, compress=None):
     """
     Overview:
         Dump tree value to file.
@@ -16,20 +33,24 @@ def dump(t: _TreeType, file, compress: Optional[Callable] = None):
     Arguments:
         - t (:obj:`_TreeType`): Tree value object.
         - file: Target dump file.
-        - compress (:obj:`Optional[Callable]`): Compress function.
+        - compress: Compress object, may be compression function, \
+            tuple of functions or module (``compress`` and ``decompress`` required).
     """
-    compress = dynamic_call(compress or (lambda x: x))
-    file.write(compress(pickle.dumps(get_data_property(t).actual())))
+    compress_func, decompress_func = _extract_compress_and_decompress(compress)
+    dump_data = (compress_func(pickle.dumps(get_data_property(t).actual())),
+                 zlib.compress(dill.dumps(decompress_func)))
+    pickle.dump(dump_data, file)
 
 
-def dumps(t: _TreeType, compress: Optional[Callable] = None) -> bytes:
+def dumps(t: _TreeType, compress=None) -> bytes:
     """
     Overview:
         Dump tree value to file.
 
     Arguments:
         - t (:obj:`_TreeType`): Tree value object.
-        - compress (:obj:`Optional[Callable]`): Compress function.
+        - compress: Compress object, may be compression function, \
+            tuple of functions or module (``compress`` and ``decompress`` required).
 
     Returns:
         - data (:obj:`bytes`): Dumped binary data.
@@ -46,15 +67,31 @@ def load(file, type_: Type[_TreeType] = TreeValue, decompress: Optional[Callable
 
     Arguments:
         - file: Original file.
-        - type\_ (:obj:`Type[_TreeType]`): Type of tree value, default is ``TreeValue``.
+        - type\\_ (:obj:`Type[_TreeType]`): Type of tree value, default is ``TreeValue``.
         - decompress (:obj:`Optional[Callable]`): Decompress function, default is ``None`` which means \
             do not do any decompression.
 
     Returns:
         - tree (:obj:`_TreeType`): Tree value object.
     """
-    decompress = dynamic_call(decompress or (lambda x: x))
-    return type_(pickle.loads(decompress(file.read())))
+    _compressed_data, _decompress_func_data = pickle.load(file)
+    decompress_func = dill.loads(zlib.decompress(_decompress_func_data))
+    if decompress_func == _NEED_DECOMPRESSION:
+        if decompress:
+            decompress_func = decompress
+        else:
+            raise RuntimeError('Decompression function not provided but it is required.')
+    elif decompress_func == _NO_DECOMPRESSION:
+        if decompress:
+            warnings.warn("Decompress function is not needed because the data is not compressed.")
+        decompress_func = lambda x: x
+    else:
+        if decompress:
+            warnings.warn("Decompress function is not needed "
+                          "because decompression function has already been provided.")
+        decompress_func = decompress_func
+
+    return type_(pickle.loads(decompress_func(_compressed_data)))
 
 
 def loads(data: Union[bytes, bytearray], type_: Optional[Type[_TreeType]] = TreeValue,
@@ -65,7 +102,7 @@ def loads(data: Union[bytes, bytearray], type_: Optional[Type[_TreeType]] = Tree
 
     Arguments:
         - data (:obj:`Union[bytes, bytearray]`): Binary data.
-        - type\_ (:obj:`Type[_TreeType]`): Type of tree value, default is ``TreeValue``.
+        - type\\_ (:obj:`Type[_TreeType]`): Type of tree value, default is ``TreeValue``.
         - decompress (:obj:`Optional[Callable]`): Decompress function, default is ``None`` which means \
             do not do any decompression.
 
