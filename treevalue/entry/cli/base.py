90787f15891c2bdf9b4114e1a4c3a61d0235f0a0
HansBug <killog@126.com>
2021-08-07 18:27:18 +0800

    optimize version output

--- treevalue/entry/cli/base.py
+++ treevalue/entry/cli/base.py
@@ -3,6 +3,23 @@ from click.core import Context, Option
 
 from ...config.meta import __TITLE__, __VERSION__, __AUTHOR__, __AUTHOR_EMAIL__
 
+_raw_authors = [item.strip() for item in __AUTHOR__.split(',') if item.strip()]
+_raw_emails = [item.strip() for item in __AUTHOR_EMAIL__.split(',')]
+if len(_raw_emails) < len(_raw_authors):
+    _raw_emails += [None] * (len(_raw_authors) - len(_raw_emails))
+elif len(_raw_emails) > len(_raw_authors):
+    _raw_emails[len(_raw_authors) - 1] = tuple(_raw_emails[len(_raw_authors) - 1:])
+    del _raw_emails[len(_raw_authors):]
+
+_author_tuples = [
+    (author, tuple([item for item in (email if isinstance(email, tuple) else ((email,) if email else ())) if item]))
+    for author, email in zip(_raw_authors, _raw_emails)
+]
+_authors = [
+    author if not emails else '{author} ({emails})'.format(author=author, emails=', '.join(emails))
+    for author, emails in _author_tuples
+]
+
 
 # noinspection PyUnusedLocal
 def print_version(ctx: Context, param: Option, value: bool) -> None:
@@ -15,7 +32,8 @@ def print_version(ctx: Context, param: Option, value: bool) -> None:
     if not value or ctx.resilient_parsing:
         return
     click.echo('{title}, version {version}.'.format(title=__TITLE__.capitalize(), version=__VERSION__))
-    click.echo('Developed by {author}, {email}.'.format(author=__AUTHOR__, email=__AUTHOR_EMAIL__))
+    if _authors:
+        click.echo('Developed by {authors}.'.format(authors=', '.join(_authors)))
     ctx.exit()
 
 
