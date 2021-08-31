3b5fade4244639340bdd47fb2cb3d839a77fb49c
HansBug <killog@126.com>
2021-08-31 22:33:31 +0800

    update demo.pys

--- docs/source/tutorials/quick_start/complex_demo.py
+++ docs/source/tutorials/quick_start/complex_demo.py
@@ -2,5 +2,9 @@ import os
 
 from dm import get_module
 
+from treevalue import TreeValue
+
 _module = get_module(os.path.abspath('create_a_complex_tree.demo.py'))
-t1, t2, t3 = _module.t1, _module.t2, _module.t3
+for key, value in _module.__dict__.items():
+    if isinstance(value, TreeValue):
+        locals()[key] = value
