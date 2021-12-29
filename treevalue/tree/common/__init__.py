007df0be72c1c9b8757aad2a779da8be4cdbafa2
HansBug <killog@126.com>
2021-12-29 13:23:57 +0800

    dev(hansbug): complete doc for delayed part

--- treevalue/tree/common/__init__.py
+++ treevalue/tree/common/__init__.py
@@ -1,3 +1,3 @@
 from .base import raw, unraw, RawWrapper
-from .delay import DelayedProxy, delayed_partial, undelay
+from .delay import DelayedProxy, delayed_partial, undelay, DelayedValueProxy, DelayedFuncProxy
 from .storage import TreeStorage, create_storage
--- treevalue/tree/common/delay.pyx
+++ treevalue/tree/common/delay.pyx
@@ -5,14 +5,47 @@ import cython
 from libcpp cimport bool
 
 cdef class DelayedProxy:
+    """
+    Overview:
+        Base class of all the delayed proxy class.
+    """
     cpdef object value(self):
+        r"""
+        Overview:
+            Get value of the delayed proxy.
+            Should make sure the result is cached.
+            Can be accessed in :func:`treevalue.tree.common.undelay` when ``is_final`` is ``False``.
+
+        Returns:
+            - value (:obj:`object`): Calculation result.
+        """
         raise NotImplementedError  # pragma: no cover
 
     cpdef object fvalue(self):
+        r"""
+        Overview:
+            Get value of the delayed proxy.
+            Can be accessed in :func:`treevalue.tree.common.undelay` when ``is_final`` is ``True``.
+
+        Returns:
+            - value (:obj:`object`): Calculation result.
+        """
         return self.value()
 
 cdef class DelayedValueProxy(DelayedProxy):
+    """
+    Overview:
+        Simple function delayed proxy.
+    """
     def __cinit__(self, object func):
+        """
+        Overview:
+            Constructor of class :class:`treevalue.tree.common.DelayedValueProxy`.
+
+        Arguments:
+            - func (:obj:`object`): Function to be called, which can be called without arguments. \
+                Delayed proxy is supported.
+        """
         self.func = func
         self.calculated = False
         self.val = None
@@ -27,7 +60,21 @@ cdef class DelayedValueProxy(DelayedProxy):
         return self.val
 
 cdef class DelayedFuncProxy(DelayedProxy):
+    """
+    Overview:
+        Simple function delayed proxy.
+    """
     def __cinit__(self, object func, tuple args, dict kwargs):
+        """
+        Overview:
+            Constructor of class :class:`treevalue.tree.common.DelayedFuncProxy`.
+
+        Arguments:
+            - func (:obj:`object`): Function to be called, which can be called with given arguments. \
+                Delayed proxy is supported.
+            - args (:obj:`tuple`): Positional arguments to be used, delayed proxy is supported.
+            - kwargs (:obj:`dict`): Key-word arguments to be used, delayed proxy is supported.
+        """
         self.func = func
         self.args = args
         self.kwargs = kwargs
@@ -60,11 +107,32 @@ cdef inline DelayedProxy _c_delayed_partial(func, args, kwargs):
     else:
         return DelayedValueProxy(func)
 
+@cython.binding(True)
 def delayed_partial(func, *args, **kwargs):
+    """
+    Overview:
+        Build a delayed partial object.
+        Similar to :func:`functools.partial`.
+
+    Returns:
+        - delayed: Delayed object.
+    """
     return _c_delayed_partial(func, args, kwargs)
 
 @cython.binding(True)
 cpdef inline object undelay(object p, bool is_final=True):
+    r"""
+    Overview:
+        Get the value of a given object, it can be a delayed proxy, a simple object or \
+            a nested delayed proxy.
+    
+    Arguments:
+        - p (:obj:`object`): Given object to be undelay.
+        - is_final (:obj:`bool`): Is final value getting or not, default is ``True``. 
+
+    Returns:
+        - value (:obj:`object): Actual value of the given ``p``.
+    """
     if isinstance(p, DelayedProxy):
         if is_final:
             return p.fvalue()
