85bc69a253edbb996608ba1f287ac83d59820a6f
HansBug <hansbug@buaa.edu.cn>
2022-01-26 12:58:34 +0800

    test(hansbug): add test for different cases

--- test/compare/facebook/test_nest.py
+++ test/compare/facebook/test_nest.py
@@ -14,11 +14,23 @@ _UMARK = pytest.mark.benchmark(group='facebook-nest') if nest is not None else p
 
 @_UMARK
 class TestCompareFacebookNest:
-    def test_nest_flatten(self, benchmark):
-        benchmark(nest.flatten, _TREE_DATA_1)
+    N = 5
 
-    def test_tv_flatten(self, benchmark):
-        benchmark(flatten, _TREE_1)
+    def __create_nested_tree_data(self, n):
+        return {
+            ('no_%04d' % (i + 1,)): _TREE_DATA_1 for i in range(n)
+        }
+
+    def __create_nested_tree(self, n):
+        return FastTreeValue(self.__create_nested_tree_data(n))
+
+    @pytest.mark.parametrize('n', [2 ** i for i in range(N)])
+    def test_nest_flatten(self, benchmark, n):
+        benchmark(nest.flatten, self.__create_nested_tree_data(n))
+
+    @pytest.mark.parametrize('n', [2 ** i for i in range(N)])
+    def test_tv_flatten(self, benchmark, n):
+        benchmark(flatten, self.__create_nested_tree(n))
 
     def test_nest_pack_as(self, benchmark):
         benchmark(nest.pack_as, _TREE_DATA_1, nest.flatten(_TREE_DATA_1))
@@ -26,11 +38,13 @@ class TestCompareFacebookNest:
     def test_tv_unflatten(self, benchmark):
         benchmark(unflatten, flatten(_TREE_1))
 
-    def test_nest_map(self, benchmark):
-        benchmark(nest.map, lambda x: x ** 2, _TREE_DATA_1)
+    @pytest.mark.parametrize('n', [2 ** i for i in range(N)])
+    def test_nest_map(self, benchmark, n):
+        benchmark(nest.map, lambda x: x ** 2, self.__create_nested_tree_data(n))
 
-    def test_tv_map(self, benchmark):
-        benchmark(mapping, _TREE_1, lambda x: x ** 2)
+    @pytest.mark.parametrize('n', [2 ** i for i in range(N)])
+    def test_tv_map(self, benchmark, n):
+        benchmark(mapping, self.__create_nested_tree(n), lambda x: x ** 2)
 
     def test_nest_map_many2(self, benchmark):
         def f(a, b):
