250316eeddb174436e1869fe24c66e85a79f6a4c
HansBug <hansbug@buaa.edu.cn>
2022-02-22 19:55:03 +0800

    release(hansbug): release as version v1.2.2

--- treevalue/config/meta.py
+++ treevalue/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = "treevalue"
 
 #: Version of this project.
-__VERSION__ = "1.2.1"
+__VERSION__ = "1.2.2"
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'A flexible, generalized tree-based data structure.'
