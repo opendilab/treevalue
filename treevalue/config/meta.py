8b489522cf68fca1b6998ef6d0089667feef2aa8
HansBug <hansbug@buaa.edu.cn>
2024-10-16 16:46:16 +0800

    dev(hansbug): use version v1.5.0

--- treevalue/config/meta.py
+++ treevalue/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = "treevalue"
 
 #: Version of this project.
-__VERSION__ = "1.4.12"
+__VERSION__ = "1.5.0"
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'A flexible, generalized tree-based data structure.'
